a9dfefc4fa440abc8fc933f85fb16675dffcf6ec
Raymond Luo <pastel@Raymonds-MacBook-Pro.local>
2022-04-01 00:00:30 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) if num <= 100 && num >= 1 => num,
+            _ => {
+                println!("Please type a number between 1-100 inclusive, ya silly goose!");
+                continue
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You goddamn genius you done did it!");
+                break;
+            }
+        }
+    }
+
+}
